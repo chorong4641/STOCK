da95c0d23da20d7e65ce685e074eb4353aef9465
e3hope <e3hope@naver.com>
2021-03-21 15:57:58 +0900

    [update]setting

--- polls/views.py
+++ polls/views.py
@@ -4,10 +4,17 @@ from django.template import loader
 from .models import Question
 
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
