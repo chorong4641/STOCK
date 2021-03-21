226f98c6ccf86140a0dccb3de39bd328529b098a
e3hope <e3hope@naver.com>
2021-03-21 12:38:16 +0900

    [update]db setting

--- STOCK/settings.py
+++ STOCK/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'STOCK.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'STOCK',
+        'USER': 'e3hope',
+        'PASSWORD': 'ds64079376*',
+        'HOST': '115.68.177.249',
+        'PORT': '3306'
     }
 }
 
