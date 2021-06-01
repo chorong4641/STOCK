c6d7bba27b38c3bec73b655943bdca8b9ce14bf6
e3hope <e3hope@naver.com>
2021-06-01 21:52:36 +0900

    유사종목검색 5개제한 / views(컨트롤러단) 파일 정리

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from . import views
+from .views import home_views, stock_views, news_views
 
 urlpatterns = [
-        path('',views.main, name='main'),
-        path('api/chart/foreign',views.foreign, name='foreign'),
-        path('api/chart/domestic',views.domestic, name='domestic'),
-        path('api/getstock/<str:stock_code>',views.getstock, name='getstock'),
-        path('api/searchstock/<str:stock_name>',views.searchstock, name='searchstock')
+        # 메인 페이지
+        # path('',main_views.main, name='main'),
+        path('api/chart/foreign',home_views.foreign, name='foreign'),
+        path('api/chart/domestic',home_views.domestic, name='domestic'),
+        path('api/searchstock/<str:stock_name>',home_views.searchstock, name='searchstock'),
+        
+        # 상세 페이지
+        path('api/getstock/<str:stock_code>',stock_views.getstock, name='getstock'),
+        
+        # 뉴스 페이지
+        # path('api/news',news_views.news, name='getstock'),
 ]
--- polls/views/home_views.py
+++ polls/views/home_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
-from .models import Stock
+from ..models import Stock
 
 import sys
 from PyQt5.QtWidgets import *
@@ -15,7 +15,7 @@ import json
 def main(request):
     return HttpResponse('main')
 
-#국내차트 데이터 전송
+#국내차트 데이터 호출
 def domestic(request):
     if request.method == 'GET':
         pythoncom.CoInitialize()
@@ -44,7 +44,7 @@ def domestic(request):
         return JsonResponse(data, json_dumps_params={'ensure_ascii': False}, status=200)
         # data = {KOSPI:[{date:20210531,index:3232},{date:20210531,index:3232}],KOSDAQ:[{date:20210531,index:3232},{date:20210531,index:3232}]}
 
-# 해외차트 데이터 전송
+# 해외차트 데이터 호출
 def foreign(request):
     if request.method == 'GET':
         pythoncom.CoInitialize()
@@ -76,72 +76,10 @@ def foreign(request):
         # data = {DOW:[{date:20210531,index:3232},{date:20210531,index:3232}],NASDAQ:[{date:20210531,index:3232},{date:20210531,index:3232}],
         #         SP500:[{date:20210531,index:3232},{date:20210531,index:3232}],SH:[{date:20210531,index:3232},{date:20210531,index:3232}]}
 
-
-# 종목 정보 전송
-def getstock(request, stock_code):
-    if request.method == 'GET':
-        # stock_code = request.GET['stock_code']
-        pythoncom.CoInitialize()
-        objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
-        bConnect = objCpCybos.IsConnect
-        if (bConnect == 0):
-            print("PLUS가 정상적으로 연결되지 않음. ")
-            exit()
-        
-        # 현재가 객체 구하기
-        objStockMst = win32com.client.Dispatch("DsCbo1.StockMst")
-        objStockMst.SetInputValue(0,'A'+stock_code)   #종목 코드 - 삼성전자
-        objStockMst.BlockRequest()
-        
-        # 현재가 통신 및 통신 에러 처리 
-        rqStatus = objStockMst.GetDibStatus()
-        rqRet = objStockMst.GetDibMsg1()
-        print("통신상태", rqStatus, rqRet)
-        if rqStatus != 0:
-            exit()
-
-        # 현재가 정보 조회
-        data = {
-            'code':objStockMst.GetHeaderValue(0),  #종목코드
-            'name':objStockMst.GetHeaderValue(1),  # 종목명
-            'time':objStockMst.GetHeaderValue(4),  # 시간
-            'closing':objStockMst.GetHeaderValue(11), # 종가
-            #'beforeafter':objStockMst.GetHeaderValue(12),  # 대비
-            'opening':objStockMst.GetHeaderValue(13),  # 시가
-            'high':objStockMst.GetHeaderValue(14),  # 고가
-            'low':objStockMst.GetHeaderValue(15),  # 저가
-            'calling':objStockMst.GetHeaderValue(16),  #매도호가
-            'offer':objStockMst.GetHeaderValue(17),   #매수호가
-            'trading_volume':objStockMst.GetHeaderValue(18),   #거래량
-            'transaction_amount':objStockMst.GetHeaderValue(19)  #거래대금
-        }
-        pythoncom.CoUninitialize()
-        return JsonResponse(data,json_dumps_params={'ensure_ascii': False}, status=200)
-        # data = {code:23,name:넷,time:~,closing:종가,beforeafter:대비,opening:시가 ,high:고가,low:저가,
-        #         calling:매도호가,offer:매수호가,trading volume:거래량,transaction_amount:거래대금}
-
-        
-        # 예상 체결관련 정보
-        # exFlag = objStockMst.GetHeaderValue(58) #예상체결가 구분 플래그
-        # exPrice = objStockMst.GetHeaderValue(55) #예상체결가
-        # exDiff = objStockMst.GetHeaderValue(56) #예상체결가 전일대비
-        # exVol = objStockMst.GetHeaderValue(57) #예상체결수량
-        # if (exFlag == ord('0')):
-        #     print("장 구분값: 동시호가와 장중 이외의 시간")
-        # elif (exFlag == ord('1')) :
-        #     print("장 구분값: 동시호가 시간")
-        # elif (exFlag == ord('2')):
-        #     print("장 구분값: 장중 또는 장종료")
-        
-        # print("예상체결가 대비 수량")
-        # print("예상체결가", exPrice)
-        # print("예상체결가 대비", exDiff)
-        # print("예상체결수량", exVol)
-
-# 종목 정보 전송
+# 종목명으로 유사 종목 검색값 호출
 def searchstock(request,stock_name):
     if request.method == 'GET':
-        queryset = Stock.objects.filter(name__contains=stock_name)
+        queryset = Stock.objects.filter(name__contains=stock_name)[:5] # 종목 유사값 최대 5개
         serialize = serializers.serialize('json', queryset)
         data = []
         
