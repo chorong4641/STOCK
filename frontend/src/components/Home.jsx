dcaf99bdd01486a9fd3374c0b5f92f4bc9c40782
chorong <chorong4641@naver.com>
2021-06-01 23:55:07 +0900

    해외 주식시장 차트 적용

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useContext, useState } from "react";
 import Chart from "react-apexcharts";
 import styled from "styled-components";
-import { Select, Input } from "antd";
 import axios from "axios";
+import { Select, Input } from "antd";
 import { store } from "../store";
-// import { getStockMarket } from "../store/actions";
+import { getStockMarket } from "../store/actions";
 
 const HomeStyled = styled.div`
-  width: 70%;
+  width: 80%;
   margin: 0 auto;
 
   .search-input {
@@ -23,10 +23,30 @@ const HomeStyled = styled.div`
     }
   }
 
-  .chart-row {
+  .market-chart {
     display: flex;
-    justify-content: space-between;
-    margin: 30px 60px;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+
+    .chart-item {
+      margin: 20px;
+      padding: 20px;
+      background-color: #fbfbfb;
+      border-radius: 5px;
+
+      .chart-text {
+        margin-bottom: 15px;
+        font-size: 1rem;
+        font-weight: 700;
+        text-align: center;
+      }
+
+      .chart-graph {
+        min-width: 450px;
+      }
+    }
   }
 `;
 
@@ -35,6 +55,7 @@ function Home(props) {
   const { Option } = Select;
   const [loading, setLoading] = useState(false);
   const [selectOption, setSelectOption] = useState({});
+  const [marketData, setMarketData] = useState({});
 
   useEffect(() => {
     onGetStockMarket();
@@ -44,77 +65,128 @@ function Home(props) {
   const onGetStockMarket = () => {
     setLoading(true);
     axios
-      .get("/polls/chart/domestic")
+      .get("/api/chart/domestic")
       .then((res) => {
         console.log("response", res);
+
+        setMarketData({ ...marketData, domestic: res.data });
       })
       .catch((error) => {
         console.log("onGetStockMarket domestic", error);
       });
 
     axios
-      .get("/polls/chart/foreign")
+      .get("/api/chart/foreign")
       .then((res) => {
         console.log("response", res);
+
+        setMarketData({ ...marketData, foreign: res.data });
         setLoading(false);
       })
       .catch((error) => {
         console.log("onGetStockMarket foreign", error);
       });
+      
+      // {
+      //   foreign: {
+      //     DOW: [
+      //       { date: 20210528, index: 34558.5 },
+      //       { date: 20210527, index: 34432.46875 },
+      //     ],
+      //     NASDAQ: [
+      //       { date: 20210529, index: 34532.46875 },
+      //       { date: 20210528, index: 34558.5 },
+      //       { date: 20210527, index: 34432.46875 },
+      //     ],
+      //     SP500: [
+      //       { date: 20210528, index: 34558.5 },
+      //       { date: 20210527, index: 34432.46875 },
+      //     ],
+      //     SH: [
+      //       { date: 20210528, index: 34558.5 },
+      //       { date: 20210527, index: 34432.46875 },
+      //     ],
+      //   },
+      // }
   };
 
   // 검색 문자열과 일치하는 주식 리스트 조회
   const onGetMatchedStock = (value) => {
     setLoading(true);
-    // const params = {
-    //   searchText: value,
-    // };
-    // axios
-    //   .get("/polls/?", { params })
-    //   .then((res) => {
-    //     console.log("response", res);
-    //     setLoading(false);
-    //   })
-    //   .catch((error) => {
-    //     console.log("onGetMatchedStock", error);
-    //   });
+
+    axios
+      .get(`/api/searchstock/${value}`)
+      .then((res) => {
+        console.log("response", res);
+
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("onGetMatchedStock", error);
+      });
   };
 
   // 종목명 검색
-  const onSearch = (value) => {
-    console.log(value);
-    onGetMatchedStock(value);
+  const onChangeSearch = (e) => {
+    onGetMatchedStock(e.target.value);
   };
 
-  const series = [
-    {
-      name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
-    },
-  ];
+  // 해외 주식 시장지수
+  const foreignData = [];
+  if (Object.keys(marketData).length > 0) {
+    Object.keys(marketData.foreign).forEach((key) => {
+      const chartY = [];
+      const chartX = [];
+      marketData.foreign[key].map((data) => {
+        chartY.push(data.index);
+        chartX.push(data.date);
+      });
 
-  const options = {
-    chart: {
-      id: "stock",
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-    },
-  };
+      let marketText = "";
+      if (key === "DOW") {
+        marketText = "다우(DOW)";
+      } else if (key === "NASDAQ") {
+        marketText = "나스닥(NASDAQ)";
+      } else if (key === "SP500") {
+        marketText = "S&P 500";
+      } else if (key === "SH") {
+        marketText = "상해(SH)";
+      }
+
+      foreignData.push({
+        text: marketText,
+        series: {
+          name: key,
+          data: chartY,
+        },
+        options: {
+          chart: {
+            id: key,
+          },
+          xaxis: {
+            categories: chartX,
+          },
+        },
+      });
+    });
+  }
 
   return (
     <HomeStyled>
       <div className="search-input">
-        <Input.Search placeholder="종목명 입력" onSearch={onSearch} onChange={onSearch} enterButton />
+        <Input.Search placeholder="종목명 입력" onChange={onChangeSearch} onSearch={onGetMatchedStock} enterButton />
       </div>
 
-      <div className="chart-row">
-        <Chart options={options} series={series} type="line" />
-        <Chart options={options} series={series} type="line" />
-      </div>
-      <div className="chart-row">
-        <Chart options={options} series={series} type="line" />
-        <Chart options={options} series={series} type="line" />
+      <div className="market-chart">
+        {foreignData.length > 0 &&
+          foreignData.map((data) => {
+            return (
+              <div className="chart-item">
+                <div className="chart-text">{data.text}</div>
+                <Chart className="chart-graph" options={data.options} series={[data.series]} type="line" />
+              </div>
+            );
+          })}
       </div>
     </HomeStyled>
   );
