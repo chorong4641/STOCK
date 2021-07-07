bda587e669a6d4f3e6241c58c217b6c3474aaf27
e3hope <e3hope@naver.com>
2021-07-07 20:35:13 +0900

    db변경(mysql->postgresql)

--- polls/models.py
+++ polls/models.py
@@ -1,27 +1,167 @@
 from django.db import models
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Board(models.Model):
+    idx = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=30, blank=True, null=True)
+    code = models.CharField(max_length=20, blank=True, null=True)
+    contents = models.CharField(max_length=200, blank=True, null=True)
+    date_insert = models.DateTimeField(blank=True, null=True)
+    date_update = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'board'
+
+
+class Bookmark(models.Model):
+    idx = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=30, blank=True, null=True)
+    code = models.CharField(max_length=20, blank=True, null=True)
+    bookmark = models.BooleanField(blank=True, null=True)
+    date_insert = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'bookmark'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Stock(models.Model):
-    code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200, blank=True, null=True)
+    field_idx = models.IntegerField(db_column='\ufeffidx', blank=True, null=True)  # Field renamed to remove unsuitable characters. Field renamed because it started with '_'.
+    code = models.CharField(max_length=20, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'stock'
 
+
 class User(models.Model):
-    idx = models.AutoField(primary_key=True)
-    id = models.CharField(max_length=30)
-    password = models.CharField(max_length=50)
-    name = models.CharField(max_length=30)
+    idx = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=30, blank=True, null=True)
+    password = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField(max_length=30, blank=True, null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
+    salt = models.CharField(max_length=200, blank=True, null=True)
     date_insert = models.DateTimeField(blank=True, null=True)
+    date_update = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'user'
 
+
 class Word(models.Model):
-    idx = models.AutoField(primary_key=True)
+    idx = models.IntegerField(primary_key=True)
     word = models.CharField(max_length=30, blank=True, null=True)
     mean = models.CharField(max_length=100, blank=True, null=True)
 
