042616de1a029b8416834a7bb1cd7250b7450ade
chorong4641 <chorong4641@naver.com>
2021-11-22 23:26:23 +0900

    종목추천 그래프 구현

--- frontend/src/components/Recommend.jsx
+++ frontend/src/components/Recommend.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
+import Chart from "react-apexcharts";
+import { store } from "../store";
+import Loading from "./Loading";
+import { addComma } from "./common/CommonFunctions";
 
 const RecommendStyled = styled.div`
   display: flex;
@@ -11,7 +16,108 @@ const RecommendStyled = styled.div`
 `;
 
 function Recommend() {
-  return <RecommendStyled>종목추천</RecommendStyled>;
+  const [loading, setLoading] = useState(false);
+  const [state, dispatch] = useContext(store);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    onGetRecommend();
+  }, []);
+
+  // 추천종목 조회
+  const onGetRecommend = async () => {
+    setLoading(true);
+
+    const params = {
+      id: state.user?.id,
+      // period: '6month', '1year', '5year'
+    };
+
+    await axios
+      .post(`/api/suggestion`, params)
+      .then((res) => {
+        setLoading(false);
+        if (res.data) {
+          const { graph, sector, company } = res.data;
+
+          // 그래프 데이터
+          const parsedGraph = JSON.parse(graph);
+
+          // 날짜 키 배열
+          const dateKeys = [];
+
+          // 섹터별 데이터
+          const seriesData = [];
+          Object.keys(parsedGraph).forEach((key) => {
+            const curDataKey = parsedGraph[key];
+            // 날짜별 시총값
+            const dataArr = [];
+            Object.keys(curDataKey).forEach((date) => {
+              dataArr.push(curDataKey[date]);
+              dateKeys.push(date);
+            });
+            seriesData.push({
+              name: key,
+              data: dataArr,
+            });
+          });
+
+          setData({
+            series: seriesData,
+            options: {
+              chart: {
+                type: "line",
+                // zoom: {
+                //   enabled: false,
+                // },
+              },
+              xaxis: {
+                categories: dateKeys,
+                tooltip: {
+                  enabled: false,
+                },
+                labels: { rotate: 0 },
+                tickAmount: 10,
+              },
+              yaxis: {
+                labels: {
+                  formatter: function (value) {
+                    return addComma(value);
+                  },
+                },
+              },
+              stroke: {
+                curve: "straight",
+                width: 2
+              },
+              colors: [
+                "#008ffb",
+                "#00e396",
+                "#feb019",
+                "#ff4560",
+                "#775dd0",
+                "#2a587b",
+                "#2c8400",
+                "#f4f868",
+                "#fc9ec9",
+                "#32fff4",
+              ],
+            },
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("onGetRecommend", error);
+      });
+  };
+
+  return (
+    <RecommendStyled>
+      <Loading loading={loading} />
+
+      {data && <Chart className="chart-graph" options={data.options} series={data.series} type="line" height="450" />}
+    </RecommendStyled>
+  );
 }
 
 export default Recommend;
