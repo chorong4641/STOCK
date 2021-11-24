ab3b14c4b0b14a9121f3fdcb6f40302c3d405d92
e3hope <e3hope@naver.com>
2021-11-24 21:31:53 +0900

    종목추천 기간별 조회기능 추가

--- polls/views/suggestion_views.py
+++ polls/views/suggestion_views.py
@@ -18,14 +18,17 @@ def suggestion(request):
             request_data = json.loads(request.body)
             # start_date = (datetime.datetime.strptime(request_data['start_date'],'%Y-%m-%d') + datetime.timedelta(1)).strftime("%Y-%m-%d")
             # end_date = (datetime.datetime.strptime(request_data['end_date'],'%Y-%m-%d') - datetime.timedelta(1)).strftime("%Y-%m-%d")
-            if request_data['period'] == '6month':
-                start_date = (datetime.datetime.strptime('2021-06-01','%Y-%m-%d') + relativedelta(months=-6)).strftime("%Y-%m-%d")
-            elif request_data['period'] == '1year':
-                start_date = (datetime.datetime.strptime('2021-06-01','%Y-%m-%d') + relativedelta(year=-1)).strftime("%Y-%m-%d")
+            if request_data['period'] == '3month':
+                start_date = (datetime.datetime.strptime('2020-12-31','%Y-%m-%d') + relativedelta(months=-3)).strftime("%Y-%m-%d")
+            elif request_data['period'] == '6month':
+                start_date = (datetime.datetime.strptime('2020-12-31','%Y-%m-%d') + relativedelta(months=-6)).strftime("%Y-%m-%d")
+            elif request_data['period'] == '9month':
+                start_date = (datetime.datetime.strptime('2020-12-31','%Y-%m-%d') + relativedelta(months=-9)).strftime("%Y-%m-%d")
             else :
-                start_date = (datetime.datetime.strptime('2021-06-01','%Y-%m-%d') + relativedelta(year=-5)).strftime("%Y-%m-%d")
-            end_date = '2021-05-30'
-            df_marcap = pd.DataFrame(list(Krx.objects.filter(date__range=[start_date,'2021-05-30']).values()))
+                start_date = (datetime.datetime.strptime('2020-12-31','%Y-%m-%d') + relativedelta(years=-1)).strftime("%Y-%m-%d")
+            
+            end_date = '2021-12-31'
+            df_marcap = pd.DataFrame(list(Krx.objects.filter(date__range=[start_date,end_date]).values()))
             # df_marcap = pd.DataFrame(list(Krx.objects.filter(date__range=['2020-01-01','2020-12-31']).values()))
             df_marcap = df_marcap.set_index('date')
             pd.options.display.float_format = '{:.1f}'.format
@@ -33,16 +36,16 @@ def suggestion(request):
             df_master = df_master.dropna(subset=['Sector'])
             df = pd.merge(df_marcap[['code', 'name', 'marcap']].reset_index(), df_master[['Symbol', 'Sector']], left_on='code', right_on="Symbol")
             marcap_sector = pd.pivot_table(df, index='date', columns='Sector', values='marcap', aggfunc='sum')
-            cols = pd.DataFrame(marcap_sector.loc[end_date].sort_values(ascending=False)).head(10).index
-            graph = marcap_sector.loc[(start_date + datetime.timedelta(1)).strftime("%Y-%m-%d"):, cols].to_json()
+            cols = pd.DataFrame(marcap_sector.loc['2020-12-30'].sort_values(ascending=False)).head(10).index
+            graph = marcap_sector.loc[(datetime.datetime.strptime(start_date,'%Y-%m-%d') + datetime.timedelta(1)).strftime("%Y-%m-%d"):, cols].to_json()
             
-            top_sector = marcap_sector.loc[end_date].sort_values(ascending=False)[:10]
+            top_sector = marcap_sector.loc['2020-12-30'].sort_values(ascending=False)[:10]
             top_sector = top_sector.to_dict()   # 1등섹터
             
             sug = []
             # 1등섹터에서 시가총액1위
             for s in top_sector.keys():
-                temp = df[(df['date'] == end_date) & (df['Sector'] == s)]
+                temp = df[(df['date'] == '2020-12-30') & (df['Sector'] == s)]
                 temp = temp.sort_values(by = ['marcap'], ascending = False).head(5)
                 sug.append({s:temp['name'].values.tolist()})
             data = {'sug':sug,'graph':graph}
